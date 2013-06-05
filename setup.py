043f0a219ea7030824ce6f31f26fcdfe70d37851
Jan Van Hees <Jakkevanhees@gmail.com>
2013-06-05 12:34:23 +0200

    new src folder and setup.py to use it

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+version = '1.0.0dev'
+
+setup(name='z3c.schema2json',
+      version=version,
+      description="Convert zope.schema defined object to dictionary structure",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Paul Wilson',
+      author_email='',
+      url='http://github.com/jakke/z3c.schema2json',
+      license='GPL',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['z3c', 'z3c.schema2json'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.interface',
+          'zope.component',
+          'zope.container',
+          'zope.schema',
+          'martian',
+          'five.grok',
+          'grokcore.component',
+          'z3c.autoinclude',
+          'z3c.schema2json',
+      ],
+      extras_require=dict(
+            test=['plone.testing',
+                  'plone.testing', ]),
+      )
